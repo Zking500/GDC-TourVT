9b5dde60a380f96d8b169120444af0f59df4e0d7
legitsauce <legitsauce3779@gmail.com>
2024-04-02 18:32:48 -0600

    ñ

---
+++ data.js
@@ -0,0 +1,173 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-test_-space-photo-1_21",
+      "name": "Test_-Space-Photo-1_21",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 720,
+      "initialViewParameters": {
+        "yaw": 0.1477539607018148,
+        "pitch": 0,
+        "fov": 1.3545525999327719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8970855163408018,
+          "pitch": 0.17374507792990457,
+          "rotation": 6.283185307179586,
+          "target": "2-test_-space-photo-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5414121798248637,
+          "pitch": -0.04892734236580765,
+          "title": "zona de programacion",
+          "text": "en esta parte los empleados se encargan de testear las pruebas de las actividades que realizamos"
+        }
+      ]
+    },
+    {
+      "id": "1-test_-space-photo-2",
+      "name": "Test_-Space-Photo-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 720,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.5557079169813166,
+          "pitch": 0.17039316206705024,
+          "title": "extencion",
+          "text": "esta es una extencion de la zona de testeo de nuestra empresa"
+        }
+      ]
+    },
+    {
+      "id": "2-test_-space-photo-3",
+      "name": "Test_-Space-Photo-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 720,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.812725262301335,
+          "pitch": 0.43848706023693573,
+          "rotation": 0,
+          "target": "3-test_-space-photo-4"
+        },
+        {
+          "yaw": 0.13018766136205073,
+          "pitch": 0.3065465403810066,
+          "rotation": 0,
+          "target": "0-test_-space-photo-1_21"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-test_-space-photo-4",
+      "name": "Test_-Space-Photo-4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 720,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0422483924372905,
+          "pitch": 0.31864626290866127,
+          "rotation": 0,
+          "target": "1-test_-space-photo-2"
+        },
+        {
+          "yaw": 2.924530012860746,
+          "pitch": 0.16471984142976837,
+          "rotation": 0,
+          "target": "2-test_-space-photo-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.13497766354903895,
+          "pitch": 0.32357534930899945,
+          "title": "descanso",
+          "text": "zona de descanso temporal para los empleados&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "zona de testing",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
